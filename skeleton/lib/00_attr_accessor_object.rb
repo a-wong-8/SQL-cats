4f3bc84f103555c2b156428d380e9a312781fe9a
a-wong-8 <awong88@berkeley.edu>
2023-05-02 15:25:36 -0700

    ..

--- skeleton/lib/00_attr_accessor_object.rb
+++ skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,8 @@
 class AttrAccessorObject
-  def self.my_attr_accessor(*names)
-    # ...
+  def self.my_attr_accessor(*names)  
+    names.each do |name|
+        name
+        
+      end
   end
 end
